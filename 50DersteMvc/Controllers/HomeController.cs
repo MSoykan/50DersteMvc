1db866c2148fac42d921b88d750a23619fdee900
MSoykan <mehmetsoykanmutlu@gmail.com>
2023-09-22 16:35:26 +0300

    Initial commit

---
+++ 50DersteMvc/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _50DersteMvc.Controllers {
+    public class HomeController : Controller {
+        public ActionResult Index() {
+            return View();
+        }
+
+        public ActionResult About() {
+            ViewBag.Message = "Aplikasyon açıklama kısmı";
+
+            return View();
+        }
+
+        public ActionResult Contact() {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Bilgi() {
+
+            return View();
+        }
+    }
+}
